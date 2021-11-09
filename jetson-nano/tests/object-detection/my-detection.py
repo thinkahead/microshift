7417571202f44ab601be1e78890624082d0faa16
Alexei Karve <karve@us.ibm.com>
2021-11-09 12:32:41 -0500

    Object Detection send image to Node Red

--- jetson-nano/tests/object-detection/my-detection.py
+++ jetson-nano/tests/object-detection/my-detection.py
@@ -3,7 +3,8 @@ import jetson.utils
 import time
 import websocket
 import os
-webSocketURL=os.getenv("WebSocketURL")
+imageUploadURL=os.getenv("ImageUploadURL",default="http://nodered2021.mybluemix.net/upload")
+webSocketURL=os.getenv("WebSocketURL",default="wss://nodered2021.mybluemix.net/ws/chat")
 videoSource=os.getenv("VideoSource",default="/dev/video0")
 #websocket.enableTrace(True)
 ws = websocket.WebSocket()
@@ -21,10 +22,13 @@ while True:
     detections = net.Detect(img)
     for detection in detections:
         if detection.ClassID==1:
-            message='{"user":"jetson","message":"%d: %s"}'%(start_time,str(detection).replace("\n",""))
+            message='{"user":"jetsonnano","message":"%d: %s"}'%(start_time,str(detection).replace("\n",""))
             print(message)
             try:
                 ws.send(message)
+                jetson.utils.cudaDeviceSynchronize()
+                jetson.utils.saveImage("101.jpg", img)
+                os.system('curl -F myFile=@101.jpg -F submit=Submit '+imageUploadURL)
             except (BrokenPipeError,websocket._exceptions.WebSocketConnectionClosedException) as e:
                 try:
                     ws.connect(webSocketURL)
