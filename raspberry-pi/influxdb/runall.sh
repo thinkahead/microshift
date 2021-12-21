7a30b512d3bc88047b13c9a3281bf83ca09b4170
Alexei Karve <karve@us.ibm.com>
2021-12-21 06:04:46 -0500

    Install InfluxDB, telegraf and Grafana in single script

---
+++ raspberry-pi/influxdb/runall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+oc new-project influxdb
+
+# InfluxDB
+oc create configmap influxdb-config --from-file=influxdb.conf
+oc apply -f influxdb-secrets.yaml
+mkdir /var/hpvolumes/influxdb
+oc apply -f influxdb-pv.yaml
+oc apply -f influxdb-data.yaml
+while [[ $(kubectl get pvc influxdb-data -o 'jsonpath={..status.phase}') != "Bound" ]]; do echo "waiting for PVC status" && sleep 1; done
+oc apply -f influxdb-deployment.yaml -f influxdb-service.yaml
+oc wait -f influxdb-deployment.yaml --for condition=available
+
+# Telegraf
+oc apply -f telegraf-config.yaml -f telegraf-secrets.yaml -f telegraf-deployment.yaml
+oc wait -f telegraf-deployment.yaml --for condition=available
+
+cd grafana
+mkdir /var/hpvolumes/grafana
+cp -r config/* /var/hpvolumes/grafana/.
+oc apply -f grafana-pv.yaml
+oc apply -f grafana-data.yaml
+while [[ $(kubectl get pvc grafana-data -o 'jsonpath={..status.phase}') != "Bound" ]]; do echo "waiting for PVC status" && sleep 1; done
+oc apply -f grafana-deployment.yaml -f grafana-service.yaml
+oc wait -f grafana-deployment.yaml --for condition=available
+oc expose svc grafana-service # Create the route
+cd ..
+
+oc get route grafana-service
